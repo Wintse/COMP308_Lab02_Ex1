d04721750d3bad1abed727247564e41daaa6ed0d
Victoria <victorialiu38@gmail.com>
2022-03-01 23:52:05 -0500

    added s

--- server/routes/student-router.js
+++ server/routes/student-router.js
@@ -8,6 +8,6 @@ router.post('/student', StudentCtrl.createStudent)
 router.put('/student/:id', StudentCtrl.updateStudent)
 router.delete('/student/:id', StudentCtrl.deleteStudent)
 router.get('/student/:id', StudentCtrl.getStudentById)
-router.get('/student', StudentCtrl.getStudents)
+router.get('/students', StudentCtrl.getStudents)
 
 module.exports = router
